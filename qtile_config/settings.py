68f6981c357ebf581660c658125be42a0fc0c1bb
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-20 22:17:26 -0600

    Remove legacy qtile_config/settings.py

--- qtile_config/settings.py
+++
@@ -1,9 +0,0 @@
-dgroups_key_binder = None
-dgroups_app_rules = []
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
-wmname = "Qtile"
