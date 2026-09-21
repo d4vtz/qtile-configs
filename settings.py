14a1054458513cb453c1341f9b4971c4d0410fda
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-20 21:22:13 -0600

    Add core Qtile settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+dgroups_key_binder = None
+dgroups_app_rules = []
+
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# Useful for a few Java applications that still inspect this value.
+wmname = "Qtile"
