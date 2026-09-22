eb49496b22c1369b19982b189aa2548c49a92b83
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 19:29:47 -0600

    feat: make Qtile session self-starting

--- dqtile/core/hooks.py
+++ dqtile/core/hooks.py
@@ -1,2 +1,9 @@
-"""Qtile lifecycle hooks."""
-from desktop.qtile.core.hooks import *
+import subprocess
+from libqtile import hook
+@hook.subscribe.startup_once
+def start_session()->None:
+    subprocess.Popen(["dbus-update-activation-environment","--systemd","WAYLAND_DISPLAY","DISPLAY","XDG_CURRENT_DESKTOP","XDG_SESSION_TYPE"])
+    subprocess.Popen(["systemctl","--user","start","qtile-session.target"])
+@hook.subscribe.shutdown
+def stop_session()->None:
+    subprocess.Popen(["systemctl","--user","stop","qtile-session.target"])
