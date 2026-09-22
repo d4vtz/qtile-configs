90971a74f817844d969105ed999eb77fc6724550
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 19:28:35 -0600

    refactor: implement dqtile/core/settings.py

--- dqtile/core/settings.py
+++ dqtile/core/settings.py
@@ -1,2 +1,15 @@
-"""Qtile runtime settings."""
-from desktop.qtile.core.settings import *
+from typing import Any
+
+dgroups_key_binder = None
+dgroups_app_rules: list[Any] = []
+follow_mouse_focus = True
+bring_front_click = "floating_only"
+floats_kept_above = True
+cursor_warp = False
+auto_fullscreen = True
+auto_minimize = False
+focus_on_window_activation = "smart"
+focus_previous_on_window_remove = True
+reconfigure_screens = True
+screen_change_debounce_timeout = 1
+wmname = "LG3D"
