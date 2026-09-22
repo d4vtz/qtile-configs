b5d72044a4f2b97982c74f6261351e4fe26749c4
d4vtz <57779526+d4vtz@users.noreply.github.com>
2026-09-21 19:28:54 -0600

    refactor: migrate dqtile/core/input.py

--- dqtile/core/input.py
+++ dqtile/core/input.py
@@ -1,8 +1,4 @@
 from libqtile.backend.wayland import InputConfig
-
-wl_input_rules = {
-    "type:touchpad": InputConfig(tap=True, drag=True, dwt=True, natural_scroll=False, scroll_method="two_finger", click_method="clickfinger"),
-    "type:keyboard": InputConfig(kb_layout="latam", kb_options="terminate:ctrl_alt_bksp"),
-}
-wl_xcursor_theme = "Papirus-Dark"
-wl_xcursor_size = 24
+wl_input_rules={"type:touchpad":InputConfig(tap=True,drag=True,dwt=True,natural_scroll=False,scroll_method="two_finger",click_method="clickfinger"),"type:keyboard":InputConfig(kb_layout="latam",kb_options="terminate:ctrl_alt_bksp")}
+wl_xcursor_theme=None
+wl_xcursor_size=24
